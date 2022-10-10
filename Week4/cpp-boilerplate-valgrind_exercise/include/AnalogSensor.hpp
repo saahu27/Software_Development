ad4899b68173a303cfd3d9819feec4d11d7a1a92
Sahruday <sahrudaypatti@gmail.com>
2022-10-09 17:53:22 -0700

    valgrind excercise

---
+++ Week4/cpp-boilerplate-valgrind_exercise/include/AnalogSensor.hpp
@@ -0,0 +1,15 @@
+/* @copyright
+ */
+
+#pragma once
+
+#include <iostream>
+
+class AnalogSensor {
+ public:
+    AnalogSensor(unsigned int samples);
+    ~AnalogSensor();
+    int Read();
+ private:
+    unsigned int mSamples;
+};
